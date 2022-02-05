1218305d61045d705138edfb8ca892b9f7dde587
nabilk11 <khan.nabiln11@gmail.com>
2022-02-04 21:20:11 -0500

    removed unused functions and objects for deployment

--- src/components/AddIssue/AddIssue.jsx
+++ src/components/AddIssue/AddIssue.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+
 
 import './addissue.css';
 
 export default function AddIssue() {
-  const [addIssue, setAddIssue] = useState(true)
+  
 
   return (
     <div className="add-issue">
--- src/components/CurrentIssue/CurrentIssue.jsx
+++ src/components/CurrentIssue/CurrentIssue.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import './currentissue.css'
 
 export default function CurrentIssue() {
-    const [addIssue, setAddIssue] = useState(true)
+    
 
   return (
   <div className="current-issue">
